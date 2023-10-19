72bcf5e1d406c07a13054ad9fa77740b6a39d971
tiger <satyam7545065@gmail.com>
2023-10-20 02:16:18 +0530

    overidden method

---
+++ src/main/java/com/driver/ClassA.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class ClassA {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
---
+++ src/main/java/com/driver/ClassB.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class ClassB extends  ClassA{
+    public String meth(){
+        return "Invoking method from class B";
+    }
+}
