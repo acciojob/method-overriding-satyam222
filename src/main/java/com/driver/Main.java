571526dcea08080787bae8d244e8960e76ff1322
tiger <satyam7545065@gmail.com>
2023-10-21 17:43:08 +0530

    changes in ref

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 public class Main {
+    public Main() {
+    }
+
     public static class A {
         public String meth(){
             return "Invoking method from class A";
@@ -13,11 +16,12 @@ public class Main {
         }
     }
      public static void main(String args[]){
-         A objA=new B();
-         System.out.println("Calling method from class A using object of class B: " + objA.meth());
+         //A objA=new B();
+         //System.out.println("Calling method from class A using object of class B: " + objA.meth());
 
          B objB = new B();//call this overridden method from obj of class B *
-         System.out.println("Calling overridden method from class B: " + objB.meth());
+         objB.meth();
+         //System.out.println("Calling overridden method from class B: " + objB.meth());
      }
   
 }
