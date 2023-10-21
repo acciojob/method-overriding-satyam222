550243a1641777e08b1d4514297e3717d4c9692d
tiger <satyam7545065@gmail.com>
2023-10-21 18:00:06 +0530

    printing mistake

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static class B extends A {
         public String meth() {
-            return "Invoking method from class B";
+            return "Method is overridden in Extendend class B";
         }
     }
      public static void main(String args[]){
@@ -21,7 +21,7 @@ public class Main {
 
          B objB = new B();//call this overridden method from obj of class B *
          objB.meth();
-         //System.out.println("Calling overridden method from class B: " + objB.meth());
+         System.out.println( objB.meth());
      }
   
 }
